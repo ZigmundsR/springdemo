f23cc5dcf2dee168f0488770344d75b8a5b7a054
ZigmundsR <78530210+ZigmundsR@users.noreply.github.com>
2023-11-06 14:05:22 +0200

    Initial spring demo commit

---
+++ src/test/java/com/zigmunds/rieksts/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zigmunds.rieksts.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
